959992a1485feb4f957749ad06a3c7acad92c563
Kristen6765 <jiahui.peng@maill.mcgill.ca>
2022-08-12 16:30:45 +0000

    databaseCreation

---
+++ PortfolioManagementProject/src/main/database/databaseCreation.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS protfolio;
+use protfolio;
+create table userAccount (
+email varchar (50) primary key not null,
+userName varchar (50),
+userPassword varchar(50));
+
+
+create table stock (
+stockID int primary key not null,
+stockName varchar (50)
+);
+
+
+
+create table TradeHistoryID (
+TradeHistoryID int primary key not null,
+purchasedPrice double,
+purchasedTime DATETIME,
+purchasedQuantities int,
+email varchar (50),
+stockID int,
+FOREIGN KEY (email) REFERENCES userAccount(email),
+FOREIGN KEY (stockID) REFERENCES stock(stockID)
+);
