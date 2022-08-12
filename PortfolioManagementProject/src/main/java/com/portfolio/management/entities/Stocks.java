5ad8b1aac349d2dc43308b342399630349e74ce6
Kristen6765 <jiahui.peng@maill.mcgill.ca>
2022-08-12 16:41:00 +0000

    Stocks class created

--- PortfolioManagementProject/src/main/java/com/portfolio/management/entities/Stocks.java
+++ PortfolioManagementProject/src/main/java/com/portfolio/management/entities/Stocks.java
@@ -1,9 +1,29 @@
 package com.portfolio.management.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-@Table
-public class Stocks {
+@Table(name="stock")
+public class Stocks implements Serializable {
+
+    public Stocks () {
+    }
+
+    public Stocks (int stockID, String stockName) {
+        this.stockID = stockID;
+        this.stockName = stockName;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="stockID")
+    private Integer stockID;
+
+
+    @Column(name="stockName")
+    private String stockName;
+
+
+
 }
