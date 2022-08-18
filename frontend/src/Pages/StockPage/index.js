f10cd11030430f46386b641b1f45609585ff3b08
Shelly <yuqing6@ualberta.ca>
2022-08-18 16:25:55 -0400

    Finish the layout of the website (hopefully)

--- frontend/src/Pages/StockPage/index.js
+++ frontend/src/Pages/StockPage/index.js
@@ -9,13 +9,17 @@ const Stock = (props) =>{
         <div style={{display: "flex"}}>
             <Sidebar title="Trade" />
             <div style={{flexGrow: 1}}>
-                <Header path="Trade"/>
-                <div className="containers-container">
-                    <div className="dashboard-container" style={{height: "200px", width: "700px"}}>
+                <Header />
+                <div className="containers-container" style={{height: "calc(100vh - 100px)", marginBottom: "5px"}}>
+                    <div className="dashboard-container" style={{height: "100%"}}>
                         <div className="dashboard-container-title">Stock Chart</div>
                     </div>
-                    <div className="dashboard-fancy-container" style={{height: "400px", width: "250px"}}>
-                        <div className="dashboard-container-title">Stocks User have</div>
+                    <div className="dashboard-fancy-container" style={{height: "100%"}}>
+                        <div className="dashboard-container-title">Details</div>
+                        <div style={{marginTop: "auto", marginBottom: "10px"}}>
+                            <div className="dashboard-fancy-container-button">Purchase</div>
+                            <div className="dashboard-fancy-container-button">Sell</div>
+                        </div>
                     </div>
                 </div>
             </div>
--- frontend/src/Pages/UserPage/index.js
+++ frontend/src/Pages/UserPage/index.js
@@ -18,22 +18,26 @@ const User = (props) =>{
     return (
         <div style={{display: "flex"}}>
             <Sidebar title="User Profile" />
-            <div style={{flexGrow: 1}}>
-                <Header path="User Profile"/>
-                <div className="containers-container">
-                    <div className="dashboard-container" style={{height: "37vh", width: "800px"}}>
-                        <div className="dashboard-container-title">Analysis Chart</div>
-                        <div>A chart to show balance changes ???</div>
+            <div style={{flexGrow: 1, marginBottom: "5px"}}>
+                <Header />
+                <div style={{height:"calc(100vh - 56px)"}}>
+                    <div className="containers-container" style={{height: "50%"}}>
+                        <div className="dashboard-container">
+                            <div className="dashboard-container-title">Analysis Chart</div>
+                            <div>A chart to show balance changes ???</div>
+                        </div>
+                        <div className="dashboard-fancy-container">
+                            <div className="dashboard-container-title">Balance</div>
+                            <div className="dashboard-container-title">$200,000</div>
+                            <div className="dashboard-fancy-container-button" style={{marginBottom: "25px", marginTop: "auto"}}>Transfer Money</div>
+                        </div>
                     </div>
-                    <div className="dashboard-fancy-container" style={{height: "37vh", width: "200px"}}>
-                        <div className="dashboard-container-title">Balance</div>
-                        <div className="dashboard-container-title">$200,000</div>
-                        <div>button here to add balance</div>
+                    <div className="containers-container" style={{height: "50%"}}>
+                        <div className="dashboard-container">
+                            <div className="dashboard-container-title">Trade History</div>
+                            <div>A table here</div>
+                        </div>
                     </div>
-                </div>
-                <div className="dashboard-container"  style={{height: "40vh", width: "1070px"}}>
-                    <div className="dashboard-container-title">Trade History</div>
-                    <div>A table here</div>
                 </div>
             </div>
         </div>
