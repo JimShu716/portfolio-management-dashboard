40a01380d3f55b17fd10586805e3872be69563a0
Kristen6765 <jiahui.peng@maill.mcgill.ca>
2022-08-25 14:56:30 +0000

    database updated

--- PortfolioManagementProject/src/main/database/databaseCreation.sql
+++ PortfolioManagementProject/src/main/database/databaseCreation.sql
@@ -3,7 +3,7 @@ use portfolio;
 
 create table userAccount (
 userID int primary key not null auto_increment,
-balance NUMERIC,
+balance decimal (20,10),
 email varchar (50) unique,
 userName varchar (50),
 userPassword varchar(50));
@@ -12,7 +12,7 @@ userPassword varchar(50));
 create table TradeHistory (
 TradeHistoryID int primary key not null auto_increment,
 property varchar (50),
-purchasedPrice NUMERIC,
+purchasedPrice decimal (20,10),
 purchasedTime varchar (50),
 purchasedQuantities int,
 userID int,
